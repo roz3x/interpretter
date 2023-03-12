844f4718ad72e480312100a9dc2bb59d9b27320e
roz3x <oroz3x@gmail.com>
2023-03-13 01:33:35 +0530

    rename to a c file

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 int main() { 
-    for (i= 0; i < 10 ; i=i+1) { 
-        for (j = 0 ; j < 10 ; j= j+1) { 
+    
+    for (i= 0; i < 10 ; i++) { 
+        for (j = 0 ; j < 10 ; j++) { 
             int t1 = 5 - i ; 
             int t2 = 5 - j ; 
             if (((t1 * t1) * (t2 * t2 )) < 10 ) { 
